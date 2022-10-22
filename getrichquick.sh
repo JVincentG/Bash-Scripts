fe8d6e08a52631119dcb503a2d55a9b5509d7521
Ragnov <vicente35713@gmail.com>
2022-10-22 22:39:49 +0800

    add 3rd task

---
+++ getrichquick.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo $RANDOM
+echo $RANDOM
+echo $RANDOM
+echo $apple
+echo "What is your name?"
+read name
+echo "How old are you?"
+read age
+
+echo "Hello, $name, you are $age years old."
