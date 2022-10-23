493e23847fc55b83ef378440ef10f9a8292fdc6f
Ragnov <vicente35713@gmail.com>
2022-10-23 10:59:09 +0800

    update script 3

--- getrichquick.sh
+++ getrichquick.sh
@@ -10,3 +10,6 @@ echo "How old are you?"
 read age
 
 echo "Hello, $name, you are $age years old."
+getrich=$((($RANDOM%15) + $age))
+
+echo "Hey, you're gonna become a millionare at $getrich years old."
